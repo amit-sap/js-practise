974cd7efa29e269353f24cf23968066bbd338772
amit-sap <161556027+amit-sap@users.noreply.github.com>
2025-05-23 07:53:09 +0000

    variable definations done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "amit@gmail.com"
+var accountPassword = "amit1234"
+accountCity = "Pune"
+let accountState
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// always try use let,not var
